2405ecb9bb048609272960e6e76e5445656b6a85
delcy <104082365+world-of-delcy@users.noreply.github.com>
2024-05-03 18:09:45 +0000

    replaced translation api with google's free translation api

--- lib/translation.ts
+++ lib/translation.ts
@@ -1,11 +1,5 @@
-import { z } from 'zod'
 import { LanguageCode } from './types'
-
-const transationResponseSchema = z.object({
-  responseData: z.object({
-    translatedText: z.string()
-  })
-})
+import { translate as translateApi } from '@vitalets/google-translate-api';
 
 export async function translate(
   query: string,
@@ -15,15 +9,8 @@ export async function translate(
   if (from === to) {
     return query
   }
-  const url = `https://api.mymemory.translated.net/get?q=${query}&langpair=${from}|${to}`
-  const response = await fetch(url)
-  if (!response.ok) {
-    return query
-  }
-  const body = await response.json()
-  const result = await transationResponseSchema.safeParseAsync(body)
-  if (!result.success) {
-    return query
-  }
-  return result.data.responseData.translatedText
+
+  const { text } = await translateApi(query, { to });
+
+  return text;
 }
